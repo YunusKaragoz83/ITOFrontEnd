d1ded0bdc2fc235ba0b0792d74077a6903f9c2b2
YunusKaragoz83 <yunuskaragoz83@gmail.com>
2025-01-31 14:20:53 +0300

    fonksiyon lab ödev sonrası komit

--- SoftITO_FrontEnd_Projelerim/Javascript/first.js
+++ SoftITO_FrontEnd_Projelerim/Javascript/first.js
@@ -3,3 +3,12 @@ console.log("Merhaba Dünya");
 console.log("Nasılsın");
 console.log("console log kısatlamsı clg+tab");
 // yeni kopyalanan dosya
+
+sayı="Ali Veli";
+console.log(sayı);
+
+
+sehir="Van"
+console.log(sehir);
+var sehir ="Mus"
+console.log(sehir);
---
+++ SoftITO_FrontEnd_Projelerim/Javascript/fonsksiyonlab.js
@@ -0,0 +1,25 @@
+ /*
+1-Verilen sayının karesini alan fonksiyon yazınız.
+2- vize1, vize2, final notu verilen öğrenciini geçme kalma durumunu dönen bir fonskiyon yazınız.
+3- Kısa ve uzun kenarları verilen bir dikdörtgenin alan ve cevre hesaplarını ekrana basan bir fonksiyon yazınız.
+ */
+
+function kareAl(x){
+    return (x*x)
+}
+
+function GecmeKalma(x,y,z){
+    return (x*0.3)+(y*0.3)+(z*0.4)
+}
+
+function Dikdörtgen(a,b){
+    alan=a*b
+    cevre=(a+b)*2
+    //return (alan,cevre)
+    console.log("Dikdörtgen alanı =",alan);
+    console.log("Dikdörtgen çevresi =", cevre);
+}
+
+console.log(kareAl(9));
+console.log(GecmeKalma(40,50,60));
+console.log(Dikdörtgen(4,5));
