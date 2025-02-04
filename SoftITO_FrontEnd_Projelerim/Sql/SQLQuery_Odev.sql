1d142a7bded6a5cfc9670af51940dfa5468421d6
YunusKaragoz83 <yunuskaragoz83@gmail.com>
2025-02-05 00:12:29 +0300

    sql odev 4 şubat aksam

---
+++ SoftITO_FrontEnd_Projelerim/Sql/SQLQuery_Odev.sql
@@ -0,0 +1,13 @@
+
+SELECT * FROM customers WHERE country='UK';
+
+SELECT * FROM orders WHERE ship_country='Germany';
+
+SELECT * FROM suppliers WHERE country='Italy';
+
+
+SELECT * FROM products 
+WHERE category_id=2
+AND unit_price>20;
+
+
